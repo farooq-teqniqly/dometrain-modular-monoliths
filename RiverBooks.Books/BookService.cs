169c3dd09a7e78cae4a25615c072fd9d1032ef9a
farooq-teqniqly <farooq@teqniqly.com>
2024-03-26 14:47:00 -0700

    Refactor

---
+++ RiverBooks.Books/BookService.cs
@@ -0,0 +1,16 @@
+﻿namespace RiverBooks.Books;
+
+internal class BookService : IBookService
+{
+    public async Task<IEnumerable<BookDto>> GetBooksAsync()
+    {
+        await Task.Delay(100);
+
+        return new[]
+        {
+            new BookDto(Guid.NewGuid(), "1984", "George Orwell"),
+            new BookDto(Guid.NewGuid(), "Brave New World", "Aldous Huxley"),
+            new BookDto(Guid.NewGuid(), "Fahrenheit 451", "Ray Bradbury")
+        };
+    }
+}
