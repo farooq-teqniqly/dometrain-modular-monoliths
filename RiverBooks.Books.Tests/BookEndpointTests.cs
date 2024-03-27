885cd678820e510493cb5631a111b81f19c2515c
farooq-teqniqly <farooq@teqniqly.com>
2024-03-27 15:52:29 -0700

    GetBooksAsync unit test

---
+++ RiverBooks.Books.Tests/BookEndpointTests.cs
@@ -0,0 +1,27 @@
+using FastEndpoints;
+using FluentAssertions;
+using NSubstitute;
+
+namespace RiverBooks.Books.Tests
+{
+    public class BookEndpointTests
+    {
+        private readonly IBookService _fakeBookService = Substitute.For<IBookService>();
+
+        [Fact]
+        public async Task Can_Get_Books()
+        {
+            _fakeBookService.GetBooksAsync().Returns(new List<BookDto>
+            {
+                new (Guid.NewGuid(), "Book 1", "Author 1"),
+                new (Guid.NewGuid(), "Book 1", "Author 1")
+            });
+
+            var endpoint = Factory.Create<ListBooksEndpoint>(_fakeBookService);
+            await endpoint.HandleAsync(CancellationToken.None);
+
+            var response = endpoint.Response;
+            response.Books.Count.Should().Be(2);
+        }
+    }
+}
---
+++ RiverBooks.Books/Meta.cs
@@ -0,0 +1,4 @@
+﻿using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("RiverBooks.Books.Tests")]
+[assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
