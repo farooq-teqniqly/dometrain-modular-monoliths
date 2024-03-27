65a25088435753ea67354aa2eadca9470d7b70a1
farooq-teqniqly <farooq@teqniqly.com>
2024-03-27 16:11:59 -0700

    Get Books endpoint end to end test.

---
+++ RiverBooks.Web.Tests/Fakes/FakeBookService.cs
@@ -0,0 +1,16 @@
+﻿using RiverBooks.Books;
+
+namespace RiverBooks.Web.Tests.Fakes;
+
+internal class FakeBookService : IBookService
+{
+    public Task<IEnumerable<BookDto>> GetBooksAsync()
+    {
+        return Task.FromResult(new List<BookDto>
+        {
+            new(Guid.NewGuid(),"Book 1", "Author 1" ),
+            new(Guid.NewGuid(),"Book 2", "Author 2" ),
+            new(Guid.NewGuid(),"Book 3", "Author 3" )
+        }.AsEnumerable());
+    }
+}
