67f723e582de770444649686f05a0a193b5dc095
farooq-teqniqly <farooq@teqniqly.com>
2024-04-01 15:52:02 -0700

    Use real db in tests

--- RiverBooks.Web.Tests/BookApiTestFixture.cs
+++ RiverBooks.Web.Tests/BookApiTestFixture.cs
@@ -6,8 +6,4 @@ namespace RiverBooks.Web.Tests;
 
 public class BookApiTestFixture : AppFixture<Program>
 {
-    protected override void ConfigureServices(IServiceCollection s)
-    {
-        s.AddScoped(_ => new FakeBookService());
-    }
 }
