a9c4f615f6814f06c06cac313c15042bdc71654b
farooq-teqniqly <farooq@teqniqly.com>
2024-04-01 15:43:30 -0700

    First EF migration

---
+++ RiverBooks.Books/Data/Migrations/20240401224018_Initial.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace RiverBooks.Books.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "Books");
+
+            migrationBuilder.CreateTable(
+                name: "Books",
+                schema: "Books",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Title = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Author = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Price = table.Column<decimal>(type: "decimal(18,6)", precision: 18, scale: 6, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Books", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                schema: "Books",
+                table: "Books",
+                columns: new[] { "Id", "Author", "Price", "Title" },
+                values: new object[,]
+                {
+                    { new Guid("4551182a-5ad4-4d10-9276-ec055719d9f3"), "Cormac McCarthy", 14.99m, "The Road" },
+                    { new Guid("7f9680cf-9130-41c4-8b88-252de10df631"), "Cormac McCarthy", 12.99m, "No Country for Old Men" },
+                    { new Guid("ec5785b5-ae50-4be4-8f58-35190fcbed9f"), "Cormac McCarthy", 9.99m, "Blood Meridian" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Books",
+                schema: "Books");
+        }
+    }
+}
