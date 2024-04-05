a950ac397d1149f379a1c04ed59e7519572cd3d4
Farooq Mahmud <mahmud.farooq@acuitybrands.com>
2024-04-05 12:03:07 -0700

    CreateBookRequest - refactor to record type.

--- RiverBooks.Books/CreateBookRequest.cs
+++ RiverBooks.Books/CreateBookRequest.cs
@@ -1,7 +1,3 @@
 ﻿namespace RiverBooks.Books;
 
-public record CreateBookRequest(Guid Id, decimal Price)
-{
-    public string Title { get; set; } = string.Empty;
-    public string Author { get; set; } = string.Empty;
-}
+public record CreateBookRequest(Guid Id, string Title, string Author, decimal Price);
