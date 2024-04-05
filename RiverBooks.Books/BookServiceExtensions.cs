3913a58f78cdc967e6d835f4f2d45b78ff3e41eb
farooq-teqniqly <farooq@teqniqly.com>
2024-04-05 13:14:29 -0700

    Configure db context for retries.

--- RiverBooks.Books/BookServiceExtensions.cs
+++ RiverBooks.Books/BookServiceExtensions.cs
@@ -9,7 +9,18 @@ public static class BookServiceExtensions
     public static IServiceCollection AddBookServices(this IServiceCollection services, ConfigurationManager configuration)
     {
         var connectionString = configuration.GetConnectionString("BooksConnectionString");
-        services.AddDbContext<BookDbContext>(options => options.UseSqlServer(connectionString));
+        services.AddDbContext<BookDbContext>(options =>
+        {
+            options.UseSqlServer(
+                connectionString,
+                sqlServerOptionsAction: sqlOptions =>
+            {
+                sqlOptions.EnableRetryOnFailure(
+                    maxRetryCount: 5,
+                    maxRetryDelay: TimeSpan.FromSeconds(30),
+                    errorNumbersToAdd: null);
+            });
+        });
 
         services.AddScoped<IBookService, BookService>();
         services.AddScoped<IBookRepository, EfBookRepository>();
