7dc29cbccf8c64c267a4c652dd81967744f00b79
farooq-teqniqly <farooq@teqniqly.com>
2024-04-06 11:34:09 -0700

    Refactor

--- RiverBooks.Users/ApplicationUser.cs
+++ RiverBooks.Users/ApplicationUser.cs
@@ -7,7 +7,7 @@ public class ApplicationUser : IdentityUser
 {
     public string FullName { get; set; } = string.Empty;
 
-    private readonly List<CartItem> _cartItems = new();
+    private readonly List<CartItem> _cartItems = [];
     public IReadOnlyCollection<CartItem> CartItems => _cartItems.AsReadOnly();
 
     public void AddItemToCart(CartItem cartItem)
@@ -25,36 +25,5 @@ public class ApplicationUser : IdentityUser
         existingItem.UpdateQuantity(existingItem.Quantity + cartItem.Quantity);
 
         //TODO: What to do if other cart item attributes have changed?
-    }
-}
-
-public class CartItem
-{
-    public Guid Id { get; private set; } = Guid.NewGuid();
-    public Guid BookId { get; private set; }
-    public string Description { get; private set; } = String.Empty;
-    public int Quantity { get; private set; }
-    public decimal UnitPrice { get; private set; }
-
-    public CartItem(Guid bookId, string description, int quantity, decimal unitPrice)
-    {
-        BookId = Guard.Against.Default(bookId);
-        Description = Guard.Against.NullOrWhiteSpace(description);
-        Quantity = Guard.Against.Negative(quantity);
-        UnitPrice = Guard.Against.Negative(unitPrice);
-    }
-
-    /// <summary>
-    /// Constructor for Entity Framework
-    /// </summary>
-    public CartItem()
-    {
-        
-    }
-
-    internal void UpdateQuantity(int newQuantity)
-    {
-        Quantity = Guard.Against.Negative(newQuantity);
-        Quantity = newQuantity;
     }
 }
---
+++ RiverBooks.Users/CartItem.cs
@@ -0,0 +1,34 @@
+﻿using Ardalis.GuardClauses;
+
+namespace RiverBooks.Users;
+
+public class CartItem
+{
+    public Guid Id { get; private set; } = Guid.NewGuid();
+    public Guid BookId { get; }
+    public string Description { get; private set; } = String.Empty;
+    public int Quantity { get; private set; }
+    public decimal UnitPrice { get; private set; }
+
+    public CartItem(Guid bookId, string description, int quantity, decimal unitPrice)
+    {
+        BookId = Guard.Against.Default(bookId);
+        Description = Guard.Against.NullOrWhiteSpace(description);
+        Quantity = Guard.Against.Negative(quantity);
+        UnitPrice = Guard.Against.Negative(unitPrice);
+    }
+
+    /// <summary>
+    /// Constructor for Entity Framework
+    /// </summary>
+    public CartItem()
+    {
+        
+    }
+
+    internal void UpdateQuantity(int newQuantity)
+    {
+        Quantity = Guard.Against.Negative(newQuantity);
+        Quantity = newQuantity;
+    }
+}
